ed445e3be85c455cfec2d24df5c45621aa952efa
Idan Moosh Haim <idanmoosh@gmail.com>
2022-09-04 17:40:36 +0300

    changed git ignore and added env file

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const loaders = require('./loaders');
+const { PORT } = require('./config');
+
+async function startServer() {
+  loaders(app);
+  app.listen(PORT, () => {
+    console.log(`server is listening on PORT ${PORT}`);
+  });
+}
+
+startServer();
